87011bc8cf2afa5deec8e944ea6a72f1d81eec0e
Naveen <guntupallinaveen999@gmail.com>
2022-04-21 11:18:26 +0530

    intial commit

---
+++ task.js
@@ -0,0 +1,79 @@
+// let btn1 = document.getElementById("bt1");
+// let addinfo = documnet.getElementById("info");
+
+// function open() {
+//     // btn1.style.display != 'none';
+
+//     if (addinfo.style.display != 'none') {
+//         addinfo.style.display = "none";
+//     }
+//     else {
+//         addinfo.style.display = "block";
+//     }
+
+// }
+
+
+function open1() {
+    let nvn1 = document.getElementById("btn1");
+    console.log(nvn1.style.display);
+    if (nvn1.style.display == "block") {
+        nvn1.style.display = "none";
+    }
+    else {
+        nvn1.style.display = "block";
+    }
+}
+
+
+function open2() {
+    let nvn2 = document.getElementById("btn2");
+    console.log(nvn2.style.display);
+    if (nvn2.style.display == "block") {
+        nvn2.style.display = "none";
+    }
+    else {
+        nvn2.style.display = "block";
+    }
+}
+
+
+function open3() {
+    let nvn3 = document.getElementById("btn3");
+    console.log(nvn3.style.display);
+    if (nvn3.style.display == "block") {
+        nvn3.style.display = "none";
+    }
+    else {
+        nvn3.style.display = "block";
+    }
+}
+
+function open4() {
+    let nvn4 = document.getElementById("btn4");
+    if (nvn4.style.display == "block") {
+
+        nvn4.style.display = 'none';
+    }
+    else {
+        nvn4.style.display = 'block';
+    }
+}
+
+function open5() {
+    let nvn5 = document.getElementById("btn5");
+    console.log(nvn5.style.display);
+    if (nvn5.style.display == "block") {
+        nvn5.style.display = "none";
+    }
+    else {
+        nvn5.style.display = "block";
+    }
+}
+
+
+
+
+
+
+
